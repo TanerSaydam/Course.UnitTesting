dfbaf8d8ea44ec03612297707c1c4d985cef24bb
tanersaydam <tanersaydam@gmail.com>
2023-12-10 03:56:53 +0300

    Parameterizing Tests

--- CalculatorLibraryTests/CalculatorTests.cs
+++ CalculatorLibraryTests/CalculatorTests.cs
@@ -1,29 +1,21 @@
 ﻿using CalculatorLibrary;
-using Xunit.Abstractions;
 
 namespace CalculatorLibraryTests;
-public class CalculatorTests : IAsyncLifetime
+public class CalculatorTests
 {
-    private readonly Calculator _sut = new();
+    private readonly Calculator _sut = new();   
 
-    private readonly ITestOutputHelper _outputHelper;
-
-    public CalculatorTests(ITestOutputHelper outputHelper)
-    {
-        _outputHelper = outputHelper;
-        _outputHelper.WriteLine("Hello from the ctor");
-    }
-
-    [Fact]
-    public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers()
+    [Theory]
+    [InlineData(5,4,9)]
+    [InlineData(0,0,0)]
+    [InlineData(-5,-5,-10)]
+    public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers(int a, int b, int expected)
     {
         // Act
-        var result = _sut.Add(5, 4);
-
-        _outputHelper.WriteLine("Hello from the add method");
+        var result = _sut.Add(a, b);
 
         // Assert
-        Assert.Equal(9, result);
+        Assert.Equal(expected, result);
     }
 
     [Fact]
@@ -32,21 +24,7 @@ public class CalculatorTests : IAsyncLifetime
         // Act
         var result = _sut.Subtract(10, 7);
 
-        _outputHelper.WriteLine("Hello from the subtract method");
-
         // Assert
         Assert.Equal(3, result);
-    }    
-
-    public async Task InitializeAsync()
-    {
-        _outputHelper.WriteLine("Hello from the initialize async");
-        Task.Delay(1);
-    }
-
-    public async Task DisposeAsync()
-    {
-        _outputHelper.WriteLine("Hello from the dispose async");
-        Task.Delay(1);
-    }
+    }  
 }
