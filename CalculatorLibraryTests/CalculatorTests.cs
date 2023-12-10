8f4a8fc7b8ac3eebeba81b6318d81f3033d4ea00
tanersaydam <tanersaydam@gmail.com>
2023-12-10 04:27:09 +0300

    Ödevin Çözümü

--- CalculatorLibraryTests/CalculatorTests.cs
+++ CalculatorLibraryTests/CalculatorTests.cs
@@ -6,9 +6,9 @@ public class CalculatorTests
     private readonly Calculator _sut = new();   
 
     [Theory]
-    [InlineData(5,4,9)]
-    [InlineData(0,0,0, Skip = "CI'da hataya sebep oluyor")]
-    [InlineData(-5,-5,-10)]
+    [InlineData(5,5,10)]
+    [InlineData(-5,5,0)]
+    [InlineData(-15,-5,-20)]
     public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers(int a, int b, int expected)
     {
         // Act
@@ -18,13 +18,44 @@ public class CalculatorTests
         Assert.Equal(expected, result);
     }
 
-    [Fact(Skip = "Şimdi değil sonra bakılacak")]
-    public void Subtract_ShouldSubtractTwoNumbers_WhenTwoNumbersAreIntegers()
+    [Theory]
+    [InlineData(5,-5,10)]
+    [InlineData(15,-5,20)]
+    [InlineData(-5,-5,0)]
+    [InlineData(-15,-5,-10)]
+    [InlineData(5,-10,15)]
+    public void Subtract_ShouldSubtractTwoNumbers_WhenTwoNumbersAreIntegers(int a, int b, int expected)
     {
         // Act
-        var result = _sut.Subtract(10, 7);
+        var result = _sut.Subtract(a, b);
 
         // Assert
-        Assert.Equal(3, result);
-    }  
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(5,5,25)]
+    [InlineData(50,0,0)]
+    [InlineData(-5,5,-25)]
+    public void Multiply_ShouldMultiplyTwoNumbers_WhenTwoNumbersAreIntefer(int a, int b, int expected)
+    {
+        // Act
+        var result = _sut.Multiply(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(5,5,1)]
+    [InlineData(15,5,3)]
+    [InlineData(0,0,0, Skip = "0 0'a bölünemez!")]
+    public void Divide_ShouldDivideTwoNumbers_WhenTwoNumbersAreIntefer(int a, int b, int expected)
+    {
+        // Act
+        var result = _sut.Divide(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
 }
