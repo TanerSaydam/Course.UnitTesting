306128239df1dedb738dbcecfe002e36af108e35
tanersaydam <tanersaydam@gmail.com>
2023-12-10 03:14:16 +0300

    Add project files.

---
+++ CalculatorLibraryTests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using CalculatorLibrary;
+
+namespace CalculatorLibraryTests;
+public class Tests
+{
+    [Fact]
+    public void TestAdd()
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(5, 4);
+
+        Assert.Equal(9, result);
+    }
+}
