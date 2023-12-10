8b756716a79353e46c9d37381ad678268560f15d
tanersaydam <tanersaydam@gmail.com>
2023-12-10 05:31:55 +0300

    String Test

--- 2.Techniques/test/TestingTechniques.Test.Unit/ValueSamplesTests.cs
+++ 2.Techniques/test/TestingTechniques.Test.Unit/ValueSamplesTests.cs
@@ -1,4 +1,19 @@
-﻿namespace TestingTechniques.Test.Unit;
+﻿using FluentAssertions;
+
+namespace TestingTechniques.Test.Unit;
 public class ValueSamplesTests
 {
+    private readonly ValueSamples _sut = new();
+
+    [Fact]
+    public void StingAssertionsExample()
+    {
+        var fullName = _sut.FullName;
+
+
+        fullName.Should().Be("Taner Saydam");
+        fullName.Should().NotBeEmpty();
+        fullName.Should().StartWith("Taner");
+        fullName.Should().EndWith("Saydam");
+    }
 }
