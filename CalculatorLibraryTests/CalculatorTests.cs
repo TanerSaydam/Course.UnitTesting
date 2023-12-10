199f2f0295b4a99e3b179064626414f04058cead
tanersaydam <tanersaydam@gmail.com>
2023-12-10 03:30:45 +0300

    Arrange, Act, Assert

--- CalculatorLibraryTests/CalculatorTests.cs
+++ CalculatorLibraryTests/CalculatorTests.cs
@@ -6,10 +6,15 @@ public class CalculatorTests
     [Fact]
     public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers()
     {
+        // Arrange
         var calculator = new Calculator();
 
+
+        // Act
         var result = calculator.Add(5, 4);
 
+
+        // Assert
         Assert.Equal(9, result);
     }
 }
