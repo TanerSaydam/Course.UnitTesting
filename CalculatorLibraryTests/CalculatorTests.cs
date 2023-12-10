6cdf825353c87675820bd578ed0ffc41c950e109
tanersaydam <tanersaydam@gmail.com>
2023-12-10 03:37:50 +0300

    xUnit Test Execution Model

--- CalculatorLibraryTests/CalculatorTests.cs
+++ CalculatorLibraryTests/CalculatorTests.cs
@@ -1,20 +1,39 @@
 ﻿using CalculatorLibrary;
+using Xunit.Abstractions;
 
 namespace CalculatorLibraryTests;
 public class CalculatorTests
 {
+    private readonly Calculator _sut = new();
+    private readonly Guid _guid = Guid.NewGuid();
+
+    private readonly ITestOutputHelper _outputHelper;
+
+    public CalculatorTests(ITestOutputHelper outputHelper)
+    {
+        _outputHelper = outputHelper;
+    }
+
     [Fact]
     public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers()
     {
-        // Arrange
-        var calculator = new Calculator();
-
-
         // Act
-        var result = calculator.Add(5, 4);
+        var result = _sut.Add(5, 4);
 
 
         // Assert
         Assert.Equal(9, result);
+    }
+
+    [Fact]
+    public void TestGuid1()
+    {
+        _outputHelper.WriteLine(_guid.ToString());
+    }
+
+    [Fact]
+    public void TestGuid2()
+    {
+        _outputHelper.WriteLine(_guid.ToString());
     }
 }
