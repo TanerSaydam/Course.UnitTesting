5bc7260014b0b8f10235b91b4e390f2b62874839
tanersaydam <tanersaydam@gmail.com>
2023-12-10 07:13:09 +0300

    Dependencies Sorunu

--- 3.Concepts/test/UnderstandingDependencies.Api.Tests.Unit/UserServiceTests.cs
+++ 3.Concepts/test/UnderstandingDependencies.Api.Tests.Unit/UserServiceTests.cs
@@ -1,4 +1,20 @@
-﻿namespace UnderstandingDependencies.Api.Tests.Unit;
+﻿using FluentAssertions;
+using UnderstandingDependecies.Api.Services;
+
+namespace UnderstandingDependencies.Api.Tests.Unit;
 public class UserServiceTests
 {
+    private readonly UserService _sut = new();
+
+    [Fact]
+    public async Task GetAllAsync_ShouldReturnEmptyList_WhenNoUsersExist()
+    {
+        // Arrange
+
+        // Act
+        var users = await _sut.GetAllAsync();
+
+        // Assert
+        users.Should().BeEmpty();
+    }
 }
